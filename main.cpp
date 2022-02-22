3fdddb1cba0cc7d1c5e7040857f2609cc585c257
RazvanDanciu28 <mariorazvan.2002@gmail.com>
2022-02-22 21:40:48 +0200

    problema facuta cu clase

--- main.cpp
+++ main.cpp
@@ -5,30 +5,79 @@ using namespace std;
 char command[10];
 float pi = 3.14;
 
-struct baza_cerc
+class baza_cerc
 {
+public:
     int raza, inaltime;
+
+    void citire_cerc(int &raza_c, int &inaltime_c){
+        cin>>raza_c;
+        cin>>inaltime_c;
+    }
+    void calcul_gem_frisca(int raza_c, int inaltime_c, float &gem, float &frisca){
+        gem = 2 * pi * raza_c * raza_c * inaltime_c;
+        frisca = 2 * pi * raza_c * inaltime_c;
+
+        cout<< "Cantitate de gem folosita pentru prajitura aceasta: " << gem<< " g" << endl;
+        cout<< "Cantitate de frisca folosita pentru prajitura aceasta: " << frisca << " g" << endl;
+    }
+
 };
-struct baza_dreptunghi
+class baza_dreptunghi
 {
+public:
     int latime, lungime, inaltime;
+
+    void citire_drept(int &lungime_d, int &latime_d, int &inaltime_d){
+        cin>>lungime_d;
+        cin>>latime_d;
+        cin>>inaltime_d;
+    }
+    void calcul_gem_frisca(int lungime_d, int latime_d, int inaltime_d, float &gem, float &frisca){
+        gem = 2 * lungime_d * latime_d * inaltime_d;
+        frisca =  2 * (lungime_d + latime_d) * inaltime_d;
+
+        cout << "Cantitate de gem folosita pentru prajitura aceasta: " << gem<< " g" << endl;
+        cout << "Cantitate de frisca folosita pentru prajitura aceasta: " << frisca << " g" << endl;
+    }
 };
-struct baza_patrat
+class baza_patrat
 {
+public:
     int latura, inaltime;
+
+    void citire_patrat(int &latura_p, int &inaltime_p){
+        cin>>latura_p>>inaltime_p;
+    }
+    void caclul_gem_frisca(int latura_p, int inaltime_p, float &gem, float &frisca){
+        gem =  2 * latura_p * latura_p * inaltime_p;
+        frisca = 4 * latura_p * inaltime_p;
+
+        cout<< "Cantitate de gem folosita pentru prajitura aceasta: " << gem<< " g" << endl;
+        cout<< "Cantitate de frisca folosita pentru prajitura aceasta: " << frisca << " g" << endl;
+    }
 };
-struct baza_triunghi
+class baza_triunghi
 {
+public:
     int cat_mica, cat_mare, inaltime;
+
+    void citire_tri(int &cat_mica_t, int &cat_mare_t, int &inaltime_t){
+        cin>>cat_mica_t>>cat_mare_t>>inaltime_t;
+    }
+    void calcul_gem_frisca(int cat_mica_t, int cat_mare_t, int inaltime_t, float &gem, float &frisca){
+        gem = cat_mare_t * cat_mica_t * inaltime_t;
+        float ipot = sqrt(cat_mare_t * cat_mare_t + cat_mica_t * cat_mica_t);
+        frisca = (ipot + cat_mare_t + inaltime_t);
+
+        cout<< "Cantitate de gem folosita pentru prajitura aceasta: " << gem<< " g" << endl;
+        cout<< "Cantitate de frisca folosita pentru prajitura aceasta: " << frisca << " g" << endl;
+    }
 };
 
-struct prajitura
+class prajitura
 {
-    bool cerc = false;
-    bool dreptunghi  = false;
-    bool patrat = false;
-    bool triunghi = false;
-
+public:
     baza_cerc forma_c;
     baza_dreptunghi forma_d;
     baza_patrat forma_p;
@@ -53,73 +102,39 @@ int main() {
             i++;
             if (baza[0] == 'c'){
                 cout << "Introdu raza si inaltimea" << endl;
-                prajituri[i].cerc = true;
-                cin>>prajituri[i].forma_c.raza;
-                cin>>prajituri[i].forma_c.inaltime;
 
-                prajituri[i].gem = 2 * pi * prajituri[i].forma_c.raza * prajituri[i].forma_c.raza * prajituri[i].forma_c.inaltime;
-                prajituri[i].frisca = 2 * pi * prajituri[i].forma_c.raza * prajituri[i].forma_c.inaltime;
+                prajituri[i].forma_c.citire_cerc(prajituri[i].forma_c.raza, prajituri[i].forma_c.inaltime);
+                prajituri[i].forma_c.calcul_gem_frisca(prajituri[i].forma_c.raza, prajituri[i].forma_c.inaltime, prajituri[i].gem, prajituri[i].frisca);
 
                 gem_total += prajituri[i].gem;
                 frisca_total += prajituri[i].frisca;
-
-                cout<< "Cantitate de gem folosita pentru prajitura aceasta: " << prajituri[i].gem<< " g" << endl;
-                cout<< "Cantitate de frisca folosita pentru prajitura aceasta: " << prajituri[i].frisca << " g" << endl;
-
-
             }
             else if (baza[0] == 'd'){
                 cout << "Introdu lungimea, latimea si inaltimea" << endl;
 
-                prajituri[i].dreptunghi = true;
-                cin>>prajituri[i].forma_d.lungime;
-                cin>>prajituri[i].forma_d.latime;
-                cin>>prajituri[i].forma_d.inaltime;
-
-                prajituri[i].gem = 2 * prajituri[i].forma_d.lungime * prajituri[i].forma_d.latime * prajituri[i].forma_d.inaltime;
-                prajituri[i].frisca = 2 * (prajituri[i].forma_d.lungime + prajituri[i].forma_d.latime) * prajituri[i].forma_d.inaltime;
+                prajituri[i].forma_d.citire_drept(prajituri[i].forma_d.lungime, prajituri[i].forma_d.latime, prajituri[i].forma_d.inaltime);
+                prajituri[i].forma_d.calcul_gem_frisca(prajituri[i].forma_d.lungime, prajituri[i].forma_d.latime, prajituri[i].forma_d.inaltime, prajituri[i].gem, prajituri[i].frisca);
 
                 gem_total += prajituri[i].gem ;
                 frisca_total += prajituri[i].frisca;
-
-                cout<< "Cantitate de gem folosita pentru prajitura aceasta: " <<prajituri[i].gem<< " g" << endl;
-                cout<< "Cantitate de frisca folosita pentru prajitura aceasta: " << prajituri[i].frisca << " g" << endl;
             }
             else if (baza[0] == 'p'){
                 cout << "Introdu latura si inaltimea" << endl;
 
-                prajituri[i].patrat = true;
-                cin>>prajituri[i].forma_p.latura;
-                cin>>prajituri[i].forma_p.inaltime;
-
-                prajituri[i].gem  = 2 * prajituri[i].forma_p.latura * prajituri[i].forma_p.latura * prajituri[i].forma_p.inaltime;
-                prajituri[i].frisca = 4 * prajituri[i].forma_p.latura * prajituri[i].forma_p.inaltime;
+                prajituri[i].forma_p.citire_patrat(prajituri[i].forma_p.latura,prajituri[i].forma_p.inaltime);
+                prajituri[i].forma_p.caclul_gem_frisca(prajituri[i].forma_p.latura, prajituri[i].forma_p.inaltime, prajituri[i].gem, prajituri[i].frisca);
 
                 gem_total += prajituri[i].gem ;
                 frisca_total += prajituri[i].frisca;
-
-                cout<< "Cantitate de gem folosita pentru prajitura aceasta: " << prajituri[i].gem << " g" << endl;
-                cout<< "Cantitate de frisca folosita pentru prajitura aceasta: " << prajituri[i].frisca << " g" << endl;
             }
             else{
                 cout << "Introdu cateta mare, cateta mica si inaltimea" << endl;
 
-                prajituri[i].triunghi = true;
-                cin>>prajituri[i].forma_t.cat_mare;
-                cin>>prajituri[i].forma_t.cat_mica;
-                cin>>prajituri[i].forma_t.inaltime;
-
-                prajituri[i].gem  = prajituri[i].forma_t.cat_mare * prajituri[i].forma_t.cat_mica * prajituri[i].forma_t.inaltime;
-                float ipot = sqrt(prajituri[i].forma_t.cat_mare * prajituri[i].forma_t.cat_mare + prajituri[i].forma_t.cat_mica * prajituri[i].forma_t.cat_mica);
-                prajituri[i].frisca = (ipot + prajituri[i].forma_t.cat_mare + prajituri[i].forma_t.inaltime);
+                prajituri[i].forma_t.citire_tri(prajituri[i].forma_t.cat_mica, prajituri[i].forma_t.cat_mare, prajituri[i].forma_t.inaltime);
+                prajituri[i].forma_t.calcul_gem_frisca(prajituri[i].forma_t.cat_mica, prajituri[i].forma_t.cat_mare, prajituri[i].forma_t.inaltime, prajituri[i].gem, prajituri[i].frisca);
 
                 gem_total += prajituri[i].gem ;
                 frisca_total += prajituri[i].frisca;
-
-                cout<< "Cantitate de gem folosita pentru prajitura aceasta: " << prajituri[i].gem << " g" << endl;
-                cout<< "Cantitate de frisca folosita pentru prajitura aceasta: " << prajituri[i].frisca << " g" << endl;
-
-
             }
         }
 
@@ -137,8 +152,6 @@ int main() {
 
             for (int j = x; j < i; j++) prajituri[j] = prajituri[j+1];
             i--;
-
-
         }
 
         cin.getline(command, 10);
